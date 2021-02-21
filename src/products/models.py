0d3e3f35b083bd736a57ae1976511a584bfe1a02
MohammadRezaQaderi <you@example.com>
2021-02-21 19:33:05 +0330

    the connection of model and setting add the models to INSTALLED_APP

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    titel       = models.CharField(max_length=120)
+    description = models.TextField(blank=True , null=True)
+    price       = models.DecimalField(max_digits=100 , decimal_places=2)
+    summary     = models.TextField(blank=False , null=False)
+    featured    = models.BooleanField()
