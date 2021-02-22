ae87d7842f502debb5ddbca74693e82d2dd4d068
MohammadRezaQaderi <you@example.com>
2021-02-22 11:36:43 +0330

    the some html file to show the view in template dir - make folder templates - set the folder in template of setting dirs - make the file of html and ... to set the view

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home_page(request ,*args , **kwargs):
+    ## the example of the simple use of httpresponse
+    # return HttpResponse("<h1>hellp world</h1>")
+
+    return render(request , 'home.html' , {})
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pages.views import home_page
 urlpatterns = [
+    path('' ,home_page , name='home'),
     path('admin/', admin.site.urls),
 ]
